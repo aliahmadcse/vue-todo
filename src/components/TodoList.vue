451ac6c4966a7253160353c3580f6df48befaa20
aliahmadcse <aliahmadcse@gmail.com>
2020-06-21 13:42:22 +0500

    Completes day 3

---
+++ src/components/TodoList.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <h1>Hello from todo list template</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            todos: [
+                {
+                    todoString: 'Write code',
+                    completed: true
+                },
+                {
+                    todoString: 'Make wordpress app',
+                    completed: false
+                },
+                {
+                    todoString: 'Drink water',
+                    completed: true
+                },
+                {
+                    todoString: 'Work on blog',
+                    completed: false
+                }
+            ]
+        };
+    },
+    methods: {
+        addTodo(newTodo) {
+            this.todos.push({
+                todoString: newTodo,
+                completed: false
+            });
+        },
+        toggleTodo(todo) {
+            todo.completed = !todo.completed;
+        },
+        editTodo(todo, newTodoString) {
+            todo.todoString = newTodoString;
+        },
+        deleteTodo(deleteTodo) {
+            this.todos = this.todos.filter(todo => todo !== deleteTodo);
+        }
+    }
+};
+</script>
+
+<style scoped>
+</style>
